a4d1c8a566713e54b71326d34e810d1329e2a1c9
NguyenTranDat <100193117+NguyenTranDat@users.noreply.github.com>
2022-05-09 23:19:02 +0700

    Add files via upload

---
+++ FinalProjectGameTertri.cpp
@@ -0,0 +1,13 @@
+﻿#include <SFML/Audio.hpp>
+#include "Game.h"
+
+
+int main(){
+    Game game;
+    
+    while (!game.gameOver()) {
+        game.update();
+    }
+    
+    if (game.gameOver()) game.closeWindow();
+}
---
+++ initGame.cpp
@@ -0,0 +1,5 @@
+#include "initGame.h"
+
+const std::string convertIntToString(int number){
+	return std::to_string(number);
+}
